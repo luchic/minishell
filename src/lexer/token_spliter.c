23bf6e859a4a23ab710a4c3d653bdd0e81b99b49
Nikita Luchinin <nluchini@3-A-10.42heilbronn.de>
2025-09-04 12:37:32 +0200

    Resolve bugs with split to strings

--- src/lexer/token_spliter.c
+++ src/lexer/token_spliter.c
@@ -26,19 +26,22 @@ static char	*get_next_token(const char **input, t_parse_mode *mode)
 	cur_input = *input;
 	while (*cur_input && !is_special_char(*cur_input))
 		cur_input++;
-	if (is_doble_char_token(cur_input))
-		cur_input++;
-	token = ft_substr(*input, 0, cur_input - *input + 1);
+	if (is_doble_char_token(cur_input) && cur_input == *input)
+		cur_input += 2;
+	if (cur_input == *input)
+		token = ft_substr(*input, 0, cur_input - *input + 1);
+	else 
+		token = ft_substr(*input, 0, cur_input - *input);
 	if (!token)
 		return (*mode = ERROR, NULL);
 	if (*cur_input == '\'')
 		*mode = SINGLE;
 	else if (*cur_input == '\"')
 		*mode = DOUBLE;
-	if (*cur_input == '\0')
-		*input = cur_input;
-	else
+	if (*cur_input == '\'' || *cur_input == '\"' || cur_input == *input )
 		*input = cur_input + 1;
+	else
+		*input = cur_input;
 	if (*mode != NORMAL)
 		return (free(token), NULL);
 	return (token);
@@ -67,6 +70,7 @@ static char	*get_double_quoted_token(const char **input, t_parse_mode *mode)
 {
 	const char	*cur_input;
 	char		*token;
+	int i;
 
 	if (!*input || !**input)
 		return (NULL);
@@ -78,6 +82,10 @@ static char	*get_double_quoted_token(const char **input, t_parse_mode *mode)
 		cur_input++;
 	}
 	token = ft_substr(*input, 0, cur_input - *input);
+	i = -1;
+	while (token && token[++i])
+		if (token[i] == '\\' && (token[i + 1] == '\"'))
+			ft_memmove(&token[i], &token[i + 1], ft_strlen(&token[i + 1]) + 1);
 	if (*cur_input == '\"')
 		*input = cur_input + 1;
 	else
