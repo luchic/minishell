78eed80e719395ebf269890da9e871266371e029
Nikita Luchinin <nluchini@3-A-10.42heilbronn.de>
2025-09-04 13:26:45 +0200

    Add validator

--- src/lexer/validate_input.c
+++ src/lexer/validate_input.c
@@ -1,5 +1,23 @@
 
+static int	is_valid(const char *input)
+{
+	int	parentheses;
+
+	parentheses = 0;
+	while (*input)
+	{
+		if (*input == '(')
+			parentheses++;
+		else if (*input == ')')
+			parentheses--;
+		if (parentheses < 0)
+			return (0);
+		input++;
+	}
+	return (parentheses == 0);
+}
+
 int	validate_parantheses(const char *input)
 {
-	return (1);
+	return (is_valid(input));
 }
