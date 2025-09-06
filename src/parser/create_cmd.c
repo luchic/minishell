f0bb047277543d3577a767a9a39bf44cdb55188b
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-06 17:36:45 +0200

    Add free t_command function

--- src/parser/create_cmd.c
+++ src/parser/create_cmd.c
@@ -2,6 +2,7 @@
 #include "ft_defines.h"
 #include "ft_lexer.h"
 #include "ft_parser.h"
+#include <stdlib.h>
 
 t_command	*create_command(t_cmd_type type)
 {
@@ -32,4 +33,33 @@ t_ast_node	*create_ast_node_assignment(t_token *token)
 	command->name = ft_strdup(token->value);
 	node->command = command;
 	return (node);
+}
+
+void	free_cmd(t_command *cmd)
+{
+	int				i;
+	t_redirection	*redir;
+	t_redirection	*tmp;
+
+	if (!cmd)
+		return ;
+	if (cmd->name)
+		free(cmd->name);
+	if (cmd->args)
+	{
+		i = -1;
+		while (cmd->args[++i])
+			free(cmd->args[i]);
+		free(cmd->args);
+	}
+	redir = cmd->redirections;
+	while (redir)
+	{
+		tmp = redir->next;
+		if (redir->value)
+			free(redir->value);
+		free(redir);
+		redir = tmp;
+	}
+	free(cmd);
 }
