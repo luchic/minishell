524038337162d24272a198bb1f81fe65698d7e27
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-07 13:47:55 +0200

    Add clone function to tokenstream

--- src/parser/token_stream.c
+++ src/parser/token_stream.c
@@ -1,5 +1,6 @@
-#include "ft_lexer.h"
-#include "ft_parser.h"
+#include "lexer.h"
+#include "parser.h"
+#include <stdlib.h>
 
 t_token	*ts_peek(t_tokenstream *ts)
 {
@@ -17,6 +18,7 @@ t_token	*ts_advance(t_tokenstream *ts)
 		ts->cur = ts->cur->next;
 	return (t);
 }
+
 int	ts_match(t_tokenstream *ts, t_token_type type)
 {
 	t_token	*t;
@@ -26,6 +28,7 @@ int	ts_match(t_tokenstream *ts, t_token_type type)
 		return (1);
 	return (0);
 }
+
 int	ts_expect(t_tokenstream *ts, t_token_type type)
 {
 	t_token	*t;
@@ -37,4 +40,21 @@ int	ts_expect(t_tokenstream *ts, t_token_type type)
 		return (1);
 	}
 	return (0);
+}
+
+t_tokenstream	*ts_clone(t_tokenstream *src)
+{
+	t_tokenstream	*clone;
+
+	clone = ft_calloc(1, sizeof(t_tokenstream));
+	if (!clone)
+		return (NULL);
+	clone->cur = src->cur;
+	return (clone);
+}
+
+void	ts_free(t_tokenstream *ts)
+{
+	if (ts)
+		free(ts);
 }
