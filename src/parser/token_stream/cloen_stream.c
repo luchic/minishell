4d89023464c1f6899001e262fc284f7d8e562b7a
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-08 12:42:38 +0200

    Refactore token stream

---
+++ src/parser/token_stream/cloen_stream.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cloen_stream.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 12:41:55 by nluchini          #+#    #+#             */
+/*   Updated: 2025/09/08 12:41:59 by nluchini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+t_tokenstream	*ts_clone(t_tokenstream *src)
+{
+	t_tokenstream	*clone;
+
+	clone = ft_calloc(1, sizeof(t_tokenstream));
+	if (!clone)
+		return (NULL);
+	clone->cur = src->cur;
+	return (clone);
+}
