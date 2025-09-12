2b04ee121b927462befdaf9992c1701e21506dc9
Mengyao Zhang <mezhang@3-A-15.42heilbronn.de>
2025-09-12 13:59:06 +0200

    only modified subshell execution

--- src/executor/exe_subsh.c
+++ src/executor/exe_subsh.c
@@ -4,46 +4,34 @@
 all executions happen in the child process
 */
 
-# include "minishell.h"
-# include "ft_defines.h"
-# include "ft_executor.h"
+#include "ft_defines.h"
+#include "ft_executor.h"
+#include "minishell.h"
 
-int execute_subshell(t_minishell *mnsh, t_subshell *subsh)
+int	execute_subshell(t_minishell *mnsh, t_subshell *subsh)
 {
-    int         status;
+	pid_t	pid;
+	int		status;
 
-    if (!mnsh || !subsh || !subsh->script)
-        return (1);
-    signal(SIGINT, SIG_DFL);// defult action: terminate the process
-    signal(SIGQUIT, SIG_DFL); //defult action: core dump
-    exit(execute_script(mnsh, subsh->script));
+	if (!mnsh || !subsh || !subsh->script)
+		return (1);
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		return (1);
+	}
+	else if (pid == 0)
+	{
+		signal(SIGINT, SIG_DFL);  // defult action: terminate the process
+		signal(SIGQUIT, SIG_DFL); // defult action: core dump
+		exit(execute_script(mnsh, subsh->script));
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		return (1);
+	}
 }
-
-// int execute_subshell(t_minishell *mnsh, t_subshell *subsh)
-// {
-//     pid_t       pid;
-//     int         status;
-
-//     if (!mnsh || !subsh || !subsh->script)
-//         return (1);
-//     pid = fork();
-//     if (pid < 0)
-//     {
-//         perror("fork");
-//         return (1);
-//     }
-//     else if (pid == 0)
-//     {
-//         signal(SIGINT, SIG_DFL);// defult action: terminate the process
-//         signal(SIGQUIT, SIG_DFL); //defult action: core dump
-//         exit(execute_script(mnsh, subsh->script));
-//     }
-//     else
-//     {
-//         waitpid(pid, &status, 0);
-//         if (WIFEXITED(status))
-//             return (WEXITSTATUS(status));
-//         return (1);
-//     }
-// }
-
