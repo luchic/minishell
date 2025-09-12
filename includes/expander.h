a52c0968e053151836a566d0ad20b991eac5af4b
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-12 21:32:44 +0200

    Edit expander's prototypes function

--- includes/expander.h
+++ includes/expander.h
@@ -6,14 +6,17 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 14:52:31 by nluchini          #+#    #+#             */
-/*   Updated: 2025/09/12 14:52:59 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/09/12 21:30:25 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPANDER_H
 # define EXPANDER_H
 
+# include "ft_defines.h"
+
 // ======================= Expander Var ========================
-void	expand_variable(t_command *cmd);
+int	run_variable_expander(t_command *cmd);
+int	run_wildcards_expander(t_command *cmd);
 
 #endif
