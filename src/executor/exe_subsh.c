e8b5fc19a7ab368293541bb5ee10d69f232f1d7e
Mengyao Zhang <mezhang@3-A-15.42heilbronn.de>
2025-09-12 13:45:01 +0200

    only modified subshell execution

--- src/executor/exe_subsh.c
+++ src/executor/exe_subsh.c
@@ -10,29 +10,40 @@ all executions happen in the child process
 
 int execute_subshell(t_minishell *mnsh, t_subshell *subsh)
 {
-    pid_t       pid;
     int         status;
 
     if (!mnsh || !subsh || !subsh->script)
         return (1);
-    pid = fork();
-    if (pid < 0)
-    {
-        perror("fork");
-        return (1);
-    }
-    else if (pid == 0)
-    {
-        signal(SIGINT, SIG_DFL);// defult action: terminate the process
-        signal(SIGQUIT, SIG_DFL); //defult action: core dump
-        exit(execute_script(mnsh, subsh->script));
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status))
-            return (WEXITSTATUS(status));
-        return (1);
-    }
+    signal(SIGINT, SIG_DFL);// defult action: terminate the process
+    signal(SIGQUIT, SIG_DFL); //defult action: core dump
+    exit(execute_script(mnsh, subsh->script));
 }
+
+// int execute_subshell(t_minishell *mnsh, t_subshell *subsh)
+// {
+//     pid_t       pid;
+//     int         status;
+
+//     if (!mnsh || !subsh || !subsh->script)
+//         return (1);
+//     pid = fork();
+//     if (pid < 0)
+//     {
+//         perror("fork");
+//         return (1);
+//     }
+//     else if (pid == 0)
+//     {
+//         signal(SIGINT, SIG_DFL);// defult action: terminate the process
+//         signal(SIGQUIT, SIG_DFL); //defult action: core dump
+//         exit(execute_script(mnsh, subsh->script));
+//     }
+//     else
+//     {
+//         waitpid(pid, &status, 0);
+//         if (WIFEXITED(status))
+//             return (WEXITSTATUS(status));
+//         return (1);
+//     }
+// }
 
