5ef4405677209b32e3ac349c8f0b68704db2d3b5
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-17 19:39:29 +0200

    Commit common headers

--- includes/ft_common.h
+++ includes/ft_common.h
@@ -4,15 +4,36 @@
 
 # include "ft_defines.h"
 # include "ft_printf.h"
+# include "lexer.h"
 # include "libft.h"
 # include "minishell.h"
+# include "parser.h"
 
 // ======================== function utils ========================
 
+/// @brief Appends a string to a dynamically allocated array of strings.
+/// @note It move only pointer's of the original strings.
+/// It dosen't duplicate them. It free only the original array of pointers.
+/// @param arr The original array of strings.
+/// @param str The string to append.
+/// @return The new array of strings, or NULL on failure.
 char	**ft_array_append(char **arr, char *str);
 void	ft_array_remove(char ***array, char *value);
 char	**ft_strdup_array(char **array);
 int		count_args(char **args);
+int		ft_append_str(char **dest, const char *src);
+
+/**
+ * @brief Inserts a string into another string at a specified position.
+ * @note It's not free str and insert, you have to free the original string.
+ * @param str The original string.
+ * @param insert The string to insert.
+ * @param pos The position to insert the string at.
+ * @return char* The new string with the inserted value, or NULL on failure.
+ */
+char	*ft_insert(char *str, char *insert, int pos);
+char	*ft_strchr_not_escaped(const char *s, int c);
+void	set_exit_code(t_minishell *mnsh, int code);
 
 // ======================== signal handling ========================
 void	init_signal_handler(void);
@@ -26,7 +47,14 @@ void	free_subshell(t_subshell *subshell);
 void	free_str_array(char **arr);
 void	free_logical(t_logical_expression *logic);
 void	free_script(t_script *script);
-void	free_minishell(t_minishell *mnsh);
+void	free_stack_minishell(t_minishell *mnsh);
 void	ft_free_redir(void *content);
+void	free_expander(void *param);
+void	free_assignment(void *param);
+void	free_cmd_expander(void *param);
+
+// ======================== Error Messages ========================
+void	msg_unexpected_token(t_token *token);
+void	msg_unexpected_token_type(t_token_type type);
 
 #endif
