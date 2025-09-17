7655c4c6cfc4c96e80b74448f9306e1a1939bad8
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-17 14:26:12 +0200

    Edit parsing subshell and pipeline

--- src/parser/parse/parse_subshell.c
+++ src/parser/parse/parse_subshell.c
@@ -6,25 +6,31 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 11:02:02 by nluchini          #+#    #+#             */
-/*   Updated: 2025/09/08 13:37:41 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/09/17 14:25:31 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-t_ast_node	*parse_subshell(t_tokenstream *ts)
+t_ast_node	*parse_subshell(t_tokenstream *ts, t_minishell *mnsh)
 {
 	t_subshell	*subshell;
 	t_ast_node	*ast_script;
 	t_ast_node	*ast_node;
 
+	ft_log_fd(LOG_INFO, STDERR, "Parse subshell\n");
 	if (!ts_expect(ts, PAREN_OPEN))
 		return (NULL);
-	ast_script = parse_script(ts);
+	ast_script = parse_script(ts, mnsh);
 	if (!ast_script)
 		return (NULL);
-	if (!ts_expect(ts, PAREN_CLOSE))
+	if (!ts_match(ts, PAREN_CLOSE))
+	{
+		msg_unexpected_token(ts_peek(ts));
+		set_exit_code(mnsh, SYNTAX_ERROR);
 		return (free_ast_tree(ast_script), NULL);
+	}
+	ts_advance(ts);
 	subshell = ft_calloc(1, sizeof(t_subshell));
 	if (!subshell)
 		return (free_ast_tree(ast_script), NULL);
@@ -32,6 +38,5 @@ t_ast_node	*parse_subshell(t_tokenstream *ts)
 	ast_node = create_ast_node(SUBSHELL);
 	if (!ast_node)
 		return (free_subshell(subshell), NULL);
-	ast_node->subshell = subshell;
-	return (ast_node);
+	return (ast_node->subshell = subshell, ast_node);
 }
