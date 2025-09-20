292361ffaa14327dc09a2d1519e0fb573838f214
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-20 11:59:05 +0200

    Add pre-parsing

---
+++ src/parser/pre_parsing/wld_utlis.c
@@ -0,0 +1,73 @@
+#include "parser.h"
+
+static void	parse_wildcards(t_exp_info *info, t_token *token,
+		int (*is_qted)(t_quote_status))
+{
+	char	*pos;
+
+	pos = ft_strchr_escaped(info->res + info->size, '*');
+	while (pos)
+	{
+		info->res[pos - info->res - 1] = SENTINEL;
+		ft_memmove(pos, pos + 1, ft_strlen(pos + 1));
+		pos = ft_strchr_escaped(pos, '*');
+	}
+	if (is_qted(token->quote_status))
+	{
+		pos = ft_strchr(info->res + info->size, '*');
+		while (pos)
+		{
+			info->res[pos - info->res] = SENTINEL;
+			pos = ft_strchr(pos + 1, '*');
+		}
+	}
+}
+
+int	update_value(t_exp_info *info, t_token *token, int is_new)
+{
+	char	*pos;
+	int		exit_status;
+
+	if (is_new)
+		info->wld_count = 0;
+	if (ft_append_str(&info->res, token->value) == -1)
+		return (-1);
+	pos = ft_strchr_not_escaped(token->value, '$');
+	while (pos)
+	{
+		exit_status = handle_var(info->size, pos, token, info->expand);
+		if (exit_status == -1)
+			return (-1);
+		pos = ft_strchr_not_escaped(pos + 1, '$');
+	}
+	parse_wildcards(info, token, is_single_quoted);
+	return (1);
+}
+
+int	set_merged_value(char **value, t_list **expander, t_tokenstream *ts)
+{
+	t_token		*token;
+	t_exp_info	info;
+
+	token = ts_peek(ts);
+	if (!token)
+		return (0);
+	ft_bzero(&info, sizeof(t_exp_info));
+	info.expand = expander;
+	if (update_value(&info, token, 1) == -1)
+		return (free(info.res), -1);
+	while (ts_match(ts, WORD))
+	{
+		token = ts_peek(ts);
+		ts_advance(ts);
+		if (!token || token->is_space_after)
+			return (*value = info.res, 1);
+		token = ts_peek(ts);
+		if (!token || !ts_match(ts, WORD))
+			break ;
+		info.size = ft_strlen(info.res);
+		if (update_value(&info, token, 0) == -1)
+			return (free(info.res), -1);
+	}
+	return (*value = info.res, 1);
+}
