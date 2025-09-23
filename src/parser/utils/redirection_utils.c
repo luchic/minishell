9b409d699d1ea7d721a3f1b0a2cc63c68cb71fc1
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-23 19:53:50 +0200

    Edit parsing. Add error handling and redirection

--- src/parser/utils/redirection_utils.c
+++ src/parser/utils/redirection_utils.c
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 10:39:22 by nluchini          #+#    #+#             */
-/*   Updated: 2025/09/17 18:45:02 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/09/23 18:29:05 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	set_redirection(t_command *cmd, t_tokenstream *ts)
 	{
 		exit_code = cmd_set_redirection(cmd, ts);
 		if (exit_code == SYNTAX_ERROR)
-			cmd->mnsh->last_exit_status = SYNTAX_ERROR;
+			return (cmd->mnsh->last_exit_status = SYNTAX_ERROR, 0);
 		if (exit_code == 0)
 			return (ft_log_fd(LOG_ERROR, ERROR, "Failed to set redirection\n"),
 				0);
