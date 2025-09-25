f2003ccbba6dde474aca8418da561f1c7e1b0d12
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-25 17:20:16 +0200

    Update signals

--- src/common/signal.c
+++ src/common/signal.c
@@ -6,43 +6,42 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 14:39:16 by mezhang           #+#    #+#             */
-/*   Updated: 2025/09/24 19:21:11 by nluchini         ###   ########.fr       */
+/*   Updated: 2025/09/25 17:19:40 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_defines.h"
 #include "ft_common.h"
+#include "ft_defines.h"
 #include "minishell.h"
 #include <readline/history.h>
 #include <readline/readline.h>
 
 void	signal_check(void)
 {
-	signal(SIGINT, SIG_DFL);  // Ctrl+C
-	signal(SIGQUIT, SIG_DFL); // core dump
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
 }
 
 void	handle_signal(int signum)
-{	
+{
 	t_minishell	*mnsh;
 
 	mnsh = *get_mnsh();
-	if (!mnsh || (signum == SIGINT && !mnsh->is_running)) //(Ctrl+C)
+	if (!mnsh || (signum == SIGINT && !mnsh->is_running))
 	{
-		write(1, "\n", 1);
+		write(mnsh->fd, "\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
 	else
-		write(1, "\n", 1);
+		write(mnsh->fd, "\n", 1);
 	if (mnsh)
 		mnsh->last_exit_status = 130;
 }
-// Handle signals for interactive mode in the main function
-//
+
 void	init_signal_handler(void)
 {
-	signal(SIGINT, handle_signal); // Ctrl+C
-	signal(SIGQUIT, SIG_IGN);      // Ctrl+\-> core dump in the child process
+	signal(SIGINT, handle_signal);
+	signal(SIGQUIT, SIG_IGN);
 }
