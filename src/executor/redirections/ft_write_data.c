c9fd9359b7cf7e64b682223989e16091e71ecc53
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-09-26 17:16:22 +0200

    Here_doc: update read data and signals

---
+++ src/executor/redirections/ft_write_data.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_write_data.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/26 17:06:57 by nluchini          #+#    #+#             */
+/*   Updated: 2025/09/26 17:10:18 by nluchini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_defines.h"
+#include "ft_executor.h"
+#include "ft_printf.h"
+#include <readline/history.h>
+#include <readline/readline.h>
+
+static void	ft_write_data_in_child(char *del, int fd)
+{
+	char		*line;
+	char		*expanded;
+
+	while (1)
+	{
+		line = get_next_line_not_in_tty();
+		if (ft_is_limiter(line, del))
+		{
+			if (line)
+				free(line);
+			break ;
+		}
+		if (!line)
+			break ;
+		expanded = expand_heredoc_line(line);
+		if (!expanded)
+			ft_printf_fd(fd, "%s\n", line);
+		else
+			ft_printf_fd(fd, "%s\n", expanded);
+		if (expanded)
+			free(expanded);
+		free(line);
+	}
+}
+
+static void	run_process_heredoc(char *del, int fd)
+{
+	t_minishell	*mnsh;
+
+	init_signal_heredoc();
+	ft_write_data_in_child(del, fd);
+	close(fd);
+	rl_clear_history();
+	mnsh = *get_mnsh();
+	if (mnsh)
+		free_stack_minishell(mnsh);
+	if (mnsh && mnsh->last_exit_status)
+		exit(mnsh->last_exit_status);
+	exit(EXIT_SUCCESS);
+}
+
+int	save_data_heredoc(char *del, int fd)
+{
+	int	pid;
+	int	status;
+
+	pid = fork();
+	if (pid == -1)
+		return (ft_log_fd(LOG_ERROR, STDERR, "heredoc: fork failed\n"),
+			EXIT_FAILURE);
+	if (pid == 0)
+		run_process_heredoc(del, fd);
+	else
+	{
+		ignore_signals();
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			ft_log_fd(LOG_ERROR, STDERR, "minishell: heredoc waitpid failed\n");
+			init_signal_handler();
+			return (EXIT_FAILURE);
+		}
+		init_signal_handler();
+		if (WEXITSTATUS(status) + 128 == 130)
+			return (WEXITSTATUS(status) + 128);
+	}
+	return (0);
+}
